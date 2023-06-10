4f28f2c0b9ba086aef003aebe7108d5938727b7f
saikrishnaamakam <saikrishnaamakam@gmail.com>
2023-06-10 19:53:14 +0530

    Initial project setup done

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    <div>
+      Hello world!
+    </div>
+  </header>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+</style>
